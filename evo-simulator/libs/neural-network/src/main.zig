8b404ee464fefa4929743713b96c65a4809959e5
Grant Greer <byteslider@proton.me>
2024-07-03 22:41:01 -0700

    feat: add functions and structs to neural-network

--- evo-simulator/libs/neural-network/src/main.zig
+++ evo-simulator/libs/neural-network/src/main.zig
@@ -1,5 +1,45 @@
 const std = @import("std");
 
-pub fn hello() void {
-    std.debug.print("hello from Neural Network!\n", .{});
-}
+const Neuron = struct {
+    bias: f32,
+    weights: []f32,
+
+    const Self = @This();
+    pub fn propagate(self: *Self, inputs: []f32) []f32 {
+        var output: f32 = 0.0;
+
+        for (self.weights, 0..) |weight, i| {
+            output += weight * inputs[i];
+        }
+        output += self.bias;
+
+        if (output > 0.0) {
+            return output;
+        } else {
+            return 0.0;
+        }
+    }
+};
+
+const Layer = struct {
+    neurons: []Neuron,
+
+    const Self = @This();
+    pub fn propagate(self: *Self, inputs: []f32, allocator: *std.mem.Allocator) ![]f32 {
+        var outputs = try allocator.alloc(f32, self.neurons.len);
+
+        for (self.neurons, 0..) |neuron, i| {
+            outputs[i] = try neuron.propagate(inputs);
+            return outputs;
+        }
+    }
+};
+
+const Network = struct {
+    layers: []Layer,
+
+    const Self = @This();
+    pub fn propagate(inputs: []f32) []f32 {
+        return inputs;
+    }
+};
